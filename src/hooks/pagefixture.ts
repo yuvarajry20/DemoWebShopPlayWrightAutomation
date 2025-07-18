5614211d27e4225d17719cf705544a3e6d89a58a
LAKSITHA29 <laksitharavi@gmail.com>
2025-07-18 22:40:35 +0530

    added a feature

--- src/hooks/pagefixture.ts
+++ src/hooks/pagefixture.ts
@@ -1,6 +1,8 @@
 import { Page } from "playwright/test";
-import {Logger} from "winston";
+import {Logger} from "winston"; 
+
 export const pageFixture = {
     page: undefined as Page | undefined,
-    logger: undefined as Logger | undefined,
+    logger: undefined as Logger | undefined
+ 
 };
