bd60b115026702d4a2477465e63dbe685b142678
LAKSITHA29 <laksitharavi@gmail.com>
2025-07-19 21:52:03 +0530

    added books feature

--- src/helper/Browser/browsermanager.ts
+++ src/helper/Browser/browsermanager.ts
@@ -1,7 +1,7 @@
 import { LaunchOptions , chromium , firefox , webkit } from "@playwright/test";
 
 const options: LaunchOptions = {
-    headless: !false
+    headless: !true
 }
 export const invokeBrowser = () => {
     const browserType = process.env.npm_config_BROWSER || 'chrome';
